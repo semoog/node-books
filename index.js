83789b8ac17ec79641c9ca77571cf621ccbf6b83
Sebastian Ernst <semo@Sebastians-MacBook-Pro.local>
2016-04-20 12:29:03 -0600

    Basic node server set up

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = 8080;
+
+var books = [{
+  title: "One Flew Over the Cuckoos Nest"
+}];
+
+app.use(bodyParser.json()); // USE: RUN THIS FUNCTION ON EVERY SINGLE REQUEST
+
+
+
+app.get('/books', function(req, res, next) {
+  res.status(200).json(books); // SEND BACK BOOKS ARRAY AS JSON
+});
+
+app.post('/books', function(req, res, next) {
+  books.push(req.body);
+  res.status(200).json(books); // SEND BACK BOOKS ARRAY AS JSON
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
