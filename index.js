157eeb2cb80298b8d6a90cab0ee166d1b9481e98
Sebastian Ernst <semo@Sebastians-MacBook-Pro.local>
2016-04-21 11:04:11 -0600

    Node params

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var app = express();
 var port = 8080;
 
 var books = [{
-  title: "One Flew Over the Cuckoos Nest"
+  title: "One Flew Over the Cuckoos Nest",
+  rating: 10
 }];
 
 app.use(bodyParser.json()); // USE: RUN THIS FUNCTION ON EVERY SINGLE REQUEST
@@ -15,17 +16,32 @@ app.use(bodyParser.json()); // USE: RUN THIS FUNCTION ON EVERY SINGLE REQUEST
 
 
 app.get('/books', function(req, res, next) {
-  res.status(200).json(books); // SEND BACK BOOKS ARRAY AS JSON
+  // SEND BACK BOOKS ARRAY AS JSON
+  console.log("getting...");
+  var rating = parseInt(req.query.rating);
+  if (rating) {
+  var result = books.filter(function (book) {
+    console.log("got.");
+    return book.rating === rating;
+  });
+  res.status(200).json(result);
+  }
+  else {
+    res.status(200).json(books);
+    console.log("got.");
+  }
 });
 
 app.post('/books', function(req, res, next) {
+  console.log("posting...");
   books.push(req.body);
   res.status(200).json(books); // SEND BACK BOOKS ARRAY AS JSON
+  console.log("posted.");
 });
 
 app.delete('/books/:id', function(req, res, next) {
-  id = parseInt(req.params.id);
-  books.slice(id, 1); // TAKES ID FROM URL AND DELETES MATCHING VALUE FROM ARRAY
+  var id = parseInt(req.params.id);
+  books.splice(id, 1); // TAKES ID FROM URL AND DELETES MATCHING VALUE FROM ARRAY
   res.status(200).send(books);
 });
 
